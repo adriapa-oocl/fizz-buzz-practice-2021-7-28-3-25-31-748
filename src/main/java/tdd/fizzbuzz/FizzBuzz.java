1dde6d796c7e59ba0ee98d3114f6762d47627001
ADRIAPA <ADRIAPA@OOCL.COM>
2021-08-05 13:50:46 +0800

    [ST004-01][ADRIAPA]test: Normal Case for input parameter

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,4 +1,8 @@
 package tdd.fizzbuzz;
 
-public class FizzBuzz {
+public class FizzBuzz{
+
+    public String countOff(String orderNumber) {
+        return null;
+    }
 }
--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,19 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class FizzBuzzTest {
+
+    @Test
+    void should_return_order_number_when_count_off_given_normal_case() {
+        //given
+        String orderNumber = "1";
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        //when
+        String result = fizzBuzz.countOff(orderNumber);
+        //then
+        assertThat(result).isEqualTo("1");
+
+    }
 }
