2e99cbe473125918cb5ca37b9ba90d09fbb8c376
ADRIAPA <ADRIAPA@OOCL.COM>
2021-08-05 13:56:49 +0800

    [ST004-01][ADRIAPA]feat: Implemented output of Fizz if count off number is divisible by 3

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -3,7 +3,10 @@ package tdd.fizzbuzz;
 public class FizzBuzz{
 
     public String countOff(String orderNumber) {
-
+        int convertedOrderNumber = Integer.parseInt(orderNumber);
+        if (convertedOrderNumber % 3 == 0){
+            return "Fizz";
+        }
         return orderNumber;
     }
 }
