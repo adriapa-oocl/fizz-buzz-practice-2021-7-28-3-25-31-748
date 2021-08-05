00a15db5422535a0230fbf842e9d29bdea4bdba8
ADRIAPA <ADRIAPA@OOCL.COM>
2021-08-05 14:26:41 +0800

    [ST004-01][ADRIAPA]refactor: changed handling for displaying of fizz buzz whizz

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -4,13 +4,27 @@ public class FizzBuzz{
 
     public String countOff(String orderNumber) {
         int convertedOrderNumber = Integer.parseInt(orderNumber);
-        if (convertedOrderNumber % 3 == 0){
-            return "Fizz";
-        }else if (convertedOrderNumber % 5 == 0){
-            return "Buzz";
-        }else if (convertedOrderNumber % 7 == 0){
-            return "Whizz";
+        if (numberFizz(convertedOrderNumber) || numberBuzz(convertedOrderNumber) || numberWhizz(convertedOrderNumber)){
+            String output = "";
+            if (numberFizz(convertedOrderNumber)){
+                output += "Fizz";
+            }if (numberBuzz(convertedOrderNumber)){
+                output += "Buzz";
+            }if (numberWhizz(convertedOrderNumber)){
+                output += "Whizz";
+            }
+            return output;
         }
         return orderNumber;
+    }
+
+    private boolean numberFizz(int number){
+        return number % 3 == 0;
+    }
+    private boolean numberBuzz(int number){
+        return number % 5 == 0;
+    }
+    private boolean numberWhizz(int number){
+        return number % 7 == 0;
     }
 }
--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -17,7 +17,7 @@ public class FizzBuzzTest {
     }
 
     @Test
-    void should_return_order_number_when_count_off_given_count_off_is_divisible_by_3() {
+    void should_return_fizz_when_count_off_given_count_off_is_divisible_by_3() {
         //given
         String orderNumber = "3";
         FizzBuzz fizzBuzz = new FizzBuzz();
@@ -28,7 +28,7 @@ public class FizzBuzzTest {
     }
 
     @Test
-    void should_return_order_number_when_count_off_given_count_off_is_divisible_by_5() {
+    void should_return_buzz_when_count_off_given_count_off_is_divisible_by_5() {
         //given
         String orderNumber = "5";
         FizzBuzz fizzBuzz = new FizzBuzz();
@@ -39,7 +39,7 @@ public class FizzBuzzTest {
     }
 
     @Test
-    void should_return_order_number_when_count_off_given_count_off_is_divisible_by_7() {
+    void should_return_whizz_when_count_off_given_count_off_is_divisible_by_7() {
         //given
         String orderNumber = "7";
         FizzBuzz fizzBuzz = new FizzBuzz();
@@ -47,5 +47,16 @@ public class FizzBuzzTest {
         String result = fizzBuzz.countOff(orderNumber);
         //then
         assertThat(result).isEqualTo("Whizz");
+    }
+
+    @Test
+    void should_return_fizz_buzz_when_count_off_given_count_off_is_divisible_by_15() {
+        //given
+        String orderNumber = "15";
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        //when
+        String result = fizzBuzz.countOff(orderNumber);
+        //then
+        assertThat(result).isEqualTo("FizzBuzz");
     }
 }
